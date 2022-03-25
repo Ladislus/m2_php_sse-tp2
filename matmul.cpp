1fb18c2c5ad155f8cc9d3acf9042f64e2abd0a0f
Ladislus <walcak.ladislas@gmail.com>
2022-03-25 19:42:23 +0100

    [MOD] Refactored / Optimized old code

--- matmul.cpp
+++ matmul.cpp
@@ -1,149 +1,126 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <immintrin.h>
+#include <iostream>
+#include <ctime>
 #include <cstring>
+#include <immintrin.h>
 
-//#define SSE
+#define SSE
 #define BONUS
 #define BLOCK_SIZE 8
 #define ELEM_SIZE 4
 
-void print_mat(float *A, size_t dim) {
-    printf("Matrice : \n");
-    for (size_t i = 0; i < dim; ++i) {
-//        printf("[ ");
-        for (size_t j = 0; j < dim; ++j) {
-            printf("%f  ", A[i * dim + j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
+void print_matrix(const float *const A, const size_t &dim, const std::string &name) {
+	std::clog << name << ": " << std::endl;
+	for (size_t i = 0; i < dim; ++i) {
+		std::clog << "[ ";
+		for (size_t j = 0; j < dim; ++j) std::clog << A[i * dim + j] << " ";
+		std::clog << "]" << std::endl;
+	}
 }
 
-void mul_drip_naive(float *A, float *B, float *res, size_t dim) {
-    /*
-     * Truc cool sur l'auto-save
-     */
-    printf("A l'appel de mul_drip_naive : \n");
-    print_mat(res, dim);
-    for (int i = 0; i < dim; ++i) {
-        for (int j = 0; j < dim; ++j) {
-            for (int k = 0; k < dim; ++k) {
-                res[i * dim + k] += A[i * dim + j] * B[j * dim + k];
-                print_mat(res, dim);
-            }
-        }
-    }
+void naive(const float *const A, const float *const B, float *const res, const size_t &dim) {
+	for (size_t i = 0; i < dim; ++i)
+		for (size_t j = 0; j < dim; ++j) {
+			float tmp_res = 0.f;
+			for (size_t k = 0; k < dim; ++k) tmp_res += A[i * dim + k] * B[k * dim + j];
+			res[i * dim + j] = tmp_res;
+		}
 }
 
-void mul_drip_sse(float *A, float *B, float *res, size_t dim) {
-    for (int i = 0; i < dim; ++i) {
-        for (int j = 0; j < dim; ++j) {
-            __m128 rA = _mm_load1_ps(A + i * dim + j);
-            for (int k = 0; k < dim; k += ELEM_SIZE) {
-                __m128 rB = _mm_load_ps(B + j * dim + k);
-                rB = _mm_mul_ps(rA, rB);
-                __m128 r_res = _mm_load_ps(res + i * dim + k);
-                r_res = _mm_add_ps(r_res, rB);
-                _mm_store_ps(res + (i * dim + k), r_res);
-            }
-        }
-    }
+void naive_drip(const float *const A, const float *const B, float *const res, const size_t &dim) {
+	// Spent 1h debugging this.
+	// We had the right answer, we just forgot to save the code
+	for (size_t i = 0; i < dim; ++i)
+		for (size_t j = 0; j < dim; ++j)
+			for (size_t k = 0; k < dim; ++k)
+				res[i * dim + k] += A[i * dim + j] * B[j * dim + k];
 }
 
-void naive_mul(float *A, float *B, float *res, size_t dim) {
-    for (size_t i = 0; i < dim; ++i) {
-        for (size_t j = 0; j < dim; ++j) {
-            int index = i * dim + j;
-            float tmp_res = 0.f;
-            for (size_t k = 0; k < dim; ++k) {
-                tmp_res += A[i * dim + k] * B[k * dim + j];
-            }
-            res[index] = tmp_res;
-        }
-    }
+void sse(const float *const A, const float *const B, float *const res, const size_t &dim) {
+	for (size_t i = 0; i < dim; ++i)
+		for (size_t j = 0; j < dim; ++j) {
+			__m128 rA = _mm_load1_ps(A + i * dim + j);
+			for (size_t k = 0; k < dim; k += ELEM_SIZE) {
+				__m128 rB = _mm_load_ps(B + j * dim + k);
+				rB = _mm_mul_ps(rA, rB);
+				__m128 rRes = _mm_load_ps(res + i * dim + k);
+				rRes = _mm_add_ps(rRes, rB);
+				_mm_store_ps(res + (i * dim + k), rRes);
+			}
+		}
 }
 
-void check(float *A, float *B, float *res, size_t dim) {
-    float *exp = (float *) malloc(dim * dim * sizeof(float));
+//void sseDrip(const float *const A, const float *const B, float *const res, const size_t &dim) {}
 
-    naive_mul(A, B, exp, dim);
-    printf("la bonne : \n");
-    print_mat(exp, dim);
+void check(const float *const A, const float *const B, const float *const res, const size_t &dim) {
+	auto *const exp = (float *) malloc(dim * dim * sizeof(float));
 
-    for (size_t i = 0; i < dim * dim; i++) {
-        if (exp[i] != res[i]) {
-            printf("Value at %lu differs: %f\n", i, exp[i] - res[i]);
-        }
-    }
+	naive(A, B, exp, dim);
+	print_matrix(exp, dim, "Expected");
+
+	for (size_t i = 0; i < (dim * dim); i++)
+		if (exp[i] != res[i])
+			std::cerr << "Value at " << i << " differs: " << exp[i] - res[i] << std::endl;
+
+	free(exp);
 }
 
 int main() {
-    size_t dim = 1 * BLOCK_SIZE;
+	const size_t dim = 1 * BLOCK_SIZE;
+	const size_t size = dim * dim;
 
-    srand(time(nullptr));
+	srand(time(nullptr));
 
 #ifdef SSE
-    float *A = (float *) _mm_malloc(dim * dim * sizeof(float), 16);
-    float *B = (float *) _mm_malloc(dim * dim * sizeof(float), 16);
-    float *C = (float *) _mm_malloc(dim * dim * sizeof(float), 16);
+	auto *const A = (float *) _mm_malloc(size * sizeof(float), 16);
+	auto *const B = (float *) _mm_malloc(size * sizeof(float), 16);
+	auto *const C = (float *) _mm_malloc(size * sizeof(float), 16);
 
-    memset(C, 0, dim * dim * sizeof(float));
+	memset(C, 0, size * sizeof(float));
 
-    for (int i = 0; i < dim * dim; ++i) {
-        A[i] = rand() % 5;
-        B[i] = rand() % 5;
-    }
-    mul_drip_sse(A, B, C, dim);
-    printf("La notre \n");
-    print_mat(C, dim);
+	for (size_t i = 0; i < size; ++i) {
+		A[i] = rand() % 5;
+		B[i] = rand() % 5;
+	}
+
+	sse(A, B, C, dim);
+	print_matrix(C, dim, "Restult");
 
 #elif defined(BONUS)
 
-    float *A = (float *) _mm_malloc(dim * dim * sizeof(float), 16);
-    float *B = (float *) _mm_malloc(dim * dim * sizeof(float), 16);
-    float *C = (float *) _mm_malloc(dim * dim * sizeof(float), 16);
+	auto *const A = (float*) _mm_malloc(size * sizeof(float), 16);
+	auto *const B = (float*) _mm_malloc(size * sizeof(float), 16);
+	auto *const C = (float*) _mm_malloc(size * sizeof(float), 16);
+	memset(C, 0, size * sizeof(float));
 
-    memset(C, 0, dim * dim * sizeof(float));
-
-    for (int i = 0; i < dim * dim; ++i) {
-        A[i] = rand() % 5;
-        B[i] = rand() % 5;
-    }
+	for (size_t i = 0; i < size; ++i) {
+		A[i] = rand() % 5;
+		B[i] = rand() % 5;
+	}
 
 #else
-    float *A = (float *) malloc(dim * dim * sizeof(float));
-    float *B = (float *) malloc(dim * dim * sizeof(float));
-    float *C = (float *) calloc(dim * dim, sizeof(float));
+	auto *const A = (float*) malloc(size * sizeof(float));
+	auto *const B = (float*) malloc(size * sizeof(float));
+	auto *const C = (float*) calloc(size, sizeof(float));
 
-    printf("Après calloc : \n");
-    print_mat(C, dim);
+	for (size_t i = 0; i < dim * dim; i++) {
+		A[i] = rand() % 5;
+		B[i] = rand() % 5;
+	}
 
-    for (size_t i = 0; i < dim * dim; i++) {
-        A[i] = rand() % 5;
-        B[i] = rand() % 5;
-    }
-    print_mat(A, dim);
-    print_mat(B, dim);
+	print_matrix(A, dim, "A");
+	print_matrix(B, dim, "B");
 
-    // your code or function here
-//    naive_mul(A, B, C, dim);
-//    print_mat(C, dim);
-
-    mul_drip_naive(A, B, C, dim);
-    print_mat(C, dim);
-
-
-// you can activate check by adding -DCHECK_MUL to your command line
+	naive_drip(A, B, C, dim);
+	print_matrix(C, dim, "C");
 #endif
+
 #ifdef CHECK_MUL
-    check(A, B, C, dim);
+	check(A, B, C, dim);
 #endif
 
-    free(A);
-    free(B);
-    free(C);
+	free(A);
+	free(B);
+	free(C);
 
-    return 0;
+	return 0;
 }
