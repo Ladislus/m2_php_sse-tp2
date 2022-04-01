5d0aafbbdc5e8fe2c4232a8aa930a3b771cc9a37
Ladislus <walcak.ladislas@gmail.com>
2022-04-01 19:20:22 +0200

    [WIP] Bonus v0.1

--- matmul.cpp
+++ matmul.cpp
@@ -9,11 +9,20 @@
 	#define LOG(code)
 #endif
 
-#define SSE
+//#define SSE
 #define BONUS
 
 #define BLOCK_SIZE 8
 #define ELEM_SIZE 4
+
+inline float reduce(__m128 reg) {
+	float elems[4];
+	_mm_store_ps(elems, reg);
+
+	float sum = 0.f;
+	for (float elem : elems) sum += elem;
+	return sum;
+}
 
 void print_matrix(const float *const A, const size_t &dim, const std::string &name) {
 	std::clog << name << ": " << std::endl;
@@ -56,8 +65,41 @@ void sse(const float *const A, const float *const B, float *const res, const siz
 		}
 }
 
-// TODO: Implement this
-//void sseDrip(const float *const A, const float *const B, float *const res, const size_t &dim) {}
+// FIXME: Wrong result
+// Result of multiplication can't be used straight away
+void sse_drip(const float *const A, const float *const B, float *const res, const size_t &dim) {
+	// Iterate over A in 4x4 blocks
+	for (size_t i = 0; i < dim; i += ELEM_SIZE)
+		for (size_t j = 0; j < dim; j += ELEM_SIZE) {
+			// Load a block from A
+			__m128 rA[4] = {
+					_mm_load_ps(A + i * dim + j),
+					_mm_load_ps(A + (i + 1) * dim + j),
+					_mm_load_ps(A + (i + 2) * dim + j),
+					_mm_load_ps(A + (i + 3) * dim + j)
+			};
+			// Iterate over B by 4x4 blocks on the same row index as A
+			for (size_t k = 0; k < dim; k += ELEM_SIZE) {
+				// Load the 4x4 block of B
+				__m128 rB[4] = {
+						_mm_load_ps(B + i * dim + k),
+						_mm_load_ps(B + (i + 1) * dim + k),
+						_mm_load_ps(B + (i + 2) * dim + k),
+						_mm_load_ps(B + (i + 3) * dim + k)
+				};
+
+				// Transpose the block (Invert rows & columns)
+				_MM_TRANSPOSE4_PS(rB[0], rB[1], rB[2], rB[3]);
+
+				// Multiply each A row by each B column and add to the result
+				for (size_t m = 0; m < ELEM_SIZE; ++m)
+					for (size_t n = 0; n < ELEM_SIZE; ++n) {
+						__m128 r = _mm_mul_ps(rA[m], rB[n]);
+						res[(i + m) * dim + (k + n)] += reduce(r);
+					}
+			}
+		}
+}
 
 void check(const float *const A, const float *const B, const float *const res, const size_t &dim) {
 	auto *const exp = (float *) malloc(dim * dim * sizeof(float));
@@ -73,7 +115,8 @@ void check(const float *const A, const float *const B, const float *const res, c
 }
 
 int main() {
-	const size_t dim = 1 * BLOCK_SIZE;
+	//constexpr size_t dim = 1 * BLOCK_SIZE;
+	constexpr size_t dim = 4;
 	const size_t size = dim * dim;
 
 	srand(time(nullptr));
@@ -82,7 +125,6 @@ int main() {
 	auto *const A = (float *) _mm_malloc(size * sizeof(float), 16);
 	auto *const B = (float *) _mm_malloc(size * sizeof(float), 16);
 	auto *const C = (float *) _mm_malloc(size * sizeof(float), 16);
-
 	memset(C, 0, size * sizeof(float));
 
 	for (size_t i = 0; i < size; ++i) {
@@ -105,6 +147,8 @@ int main() {
 		B[i] = rand() % 5;
 	}
 
+	sse_drip(A, B, C, dim);
+	LOG(print_matrix(C, dim, "Result");)
 #else
 	auto *const A = (float*) malloc(size * sizeof(float));
 	auto *const B = (float*) malloc(size * sizeof(float));
